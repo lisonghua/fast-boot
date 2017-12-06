8e616b6bc48d69b72f52a9c163142f11fe0e2edd
lisonghua <sngh_lee@sina.com>
2017-12-06 21:51:55 +0800

    refactor Signed-off-by: lisonghua <sngh_lee@sina.com>

---
+++ fast-boot-sample/vote-web/target/classes/static/app/js/page-header-vue.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+Vue.component('link-css',{
+	template:'<link href="/plugins/bootstrap-3.3.0/css/bootstrap.min.css" rel="stylesheet"/>'
+		+'<link href="/plugins/material-design-iconic-font-2.2.0/css/material-design-iconic-font.min.css"rel="stylesheet"/>'
+		+'<link href="/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css" rel="stylesheet"/>'
+		+'<link href="/plugins/bootstrap-table-1.11.0/bootstrap-table.min.css" rel="stylesheet"/>'
+		+'<link href="/plugins/waves-0.7.5/waves.min.css" rel="stylesheet"/>'
+		+'<link href="/plugins/jquery-confirm/jquery-confirm.min.css" rel="stylesheet"/>'
+		+'<link href="https://cdn.bootcss.com/select2/4.0.3/css/select2.min.css" rel="stylesheet"/>'
+		+'<link href="/app/css/common.css" rel="stylesheet"/>'
+	
+});
+var fastApp=new Vue({
+	el:'#fast-app'
+});
--- fast-boot-security-web/src/main/java/com/lish/dongfang/security/FastBootSecurityApplication.java
+++ fast-boot-security-web/src/main/java/com/lish/dongfang/security/FastBootSecurityApplication.java
@@ -9,10 +9,9 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
  * @author lisong
  *单独运行请打开注释
  */
-//@SpringBootApplication
-//@ServletComponentScan
+@SpringBootApplication
+@ServletComponentScan
 public class FastBootSecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FastBootSecurityApplication.class, args);
 	}
